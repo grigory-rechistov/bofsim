ceebf970eaac73bd5b91370ca53fa7736e4e60ea
Grigory Rechistov <grigory.rechistov@phystech.edu>
2014-11-10 22:41:33 +0300

    Separate file for expecting functions

---
+++ test/expect.h
@@ -0,0 +1,51 @@
+/* Copyright (c) 2014, Grigory Rechistov
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef EXPECT_H_
+#define EXPECT_H_
+
+static inline void TestExpectTrue(bool condition, 
+                                  const std::string descr) {
+    std::exception e;
+    if (!condition) {
+        std::cerr << "Condition failed: " << descr << std::endl;
+        throw e;
+    }
+}
+
+static inline void TestExpectEqual(my_uint128_t a, 
+                                   my_uint128_t b, 
+                                   const std::string descr) {
+    std::exception e;
+    if (a != b) {
+        std::cerr << "Equalty failed: expected " 
+                  << a << ", got " << b << std::endl
+                  << descr << std::endl;
+        throw e;
+    }
+}
+
+#endif  // EXPECT_H_
