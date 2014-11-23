f1a12738c553a21b40b9ac86cacb8bd25152b314
Grigory Rechistov <grigory.rechistov@phystech.edu>
2014-11-24 00:56:03 +0300

    Added test for "<" violation

---
+++ test/test-cpu-left-02.cpp
@@ -0,0 +1,58 @@
+// Unit test to check '<' instruction violation
+
+#include <exception>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <istream>
+
+#include "expect.h"
+#include "memory.h"
+#include "bofsim.h"
+#include "iodev.h"
+#include "config.h"
+
+#define BUFSIZE 4096
+
+int main() {
+    Configuration cpuCfg;
+    cpuCfg.cfg = { {"tl", 10},
+                   {"tw", 8},
+                   {"nm", 3},
+                   {"sd", 1},
+                   {"il", BUFSIZE}
+    };
+    
+    /* Add Objects */
+    Memory tape("tape");
+    Memory acodeInstr("ainstr");
+    Memory scodeInstr("sinstr");
+    IODev  io("io");
+    BfCpu  cpu("cpu", cpuCfg, tape, acodeInstr, scodeInstr, io);
+
+    /* Initialize acodeInstr */
+    std::vector<char> buf(BUFSIZE);
+    buf.assign(BUFSIZE,'<');
+    acodeInstr.LoadRaw(buf.data(), BUFSIZE);
+    
+    /* Initialize Tape */
+    buf[0] = 'a';
+    tape.LoadRaw(buf.data(), 1024);
+    
+    // Adjust TP to point to end of tape
+    cpu.SetRegister("tp", 0);
+    
+    Configuration cpuregs;
+    /* Do simulation */
+    
+    std::cout << "Before:\n" << cpu.GetRegs().Dump();
+    my_uint128_t old_tp = cpu.GetRegs().cfg["tp"];
+    cpu.ExecuteOneStep();
+    std::cout << "After:\n" << cpu.GetRegs().Dump();
+    cpuregs = cpu.GetRegs();
+    TestExpectEqual(old_tp, cpuregs.cfg["tp"], "TP hasn't changed");
+    TestExpectEqual(0x0, cpuregs.cfg["pc"], "PC is reset");
+    TestExpectEqual(0x1613C, cpuregs.cfg["sr"], "SR is correct");
+    
+    return 0;
+}
